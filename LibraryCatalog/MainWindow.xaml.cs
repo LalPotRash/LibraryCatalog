b7670c224c48057a3df37a9269a463928cc2c18e
Даниил Канев <daniil.kanev1994@gmail.com>
2021-06-25 15:19:02 +0300

    add menu buttons

--- LibraryCatalog/MainWindow.xaml.cs
+++ LibraryCatalog/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace LibraryCatalog
 {
@@ -20,6 +22,8 @@ namespace LibraryCatalog
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -44,6 +48,31 @@ namespace LibraryCatalog
             BookGrid.Visibility = Visibility.Hidden;
             PuzzleGrid.Visibility = Visibility.Hidden;
             TableGrid.Visibility = Visibility.Visible;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, "testTEXT");
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                File.ReadAllText(openFileDialog.FileName);
+                filePath = openFileDialog.FileName;
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePath != null)
+                File.Delete(filePath);
         }
     }
 }
